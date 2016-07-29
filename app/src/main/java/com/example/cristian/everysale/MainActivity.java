4d043d06a5d26bf239c940230ec1b67441c537cf
swario <pentolaq-o-ulix@hotmail.it>
2016-07-29 13:58:43 +0200

    swario 13:57 creato frag login modificato e adeguato main per entrare nel frag

--- app/src/main/java/com/example/cristian/everysale/MainActivity.java
+++ app/src/main/java/com/example/cristian/everysale/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.cristian.everysale;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button loginButton;
+    private Button registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get reference to the wigets
+        loginButton = (Button) findViewById(R.id.loginButton);
+        registerButton = (Button) findViewById(R.id.registerButton);
+
+        //set listener
+        loginButton.setOnClickListener(this);
+        registerButton.setOnClickListener(this);
     }
+
+    //implement listener
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.loginButton:
+                setContentView(R.layout.fragment_login);
+                break;
+            case R.id.registerButton:
+                setContentView(R.layout.fragment_register_1);
+                break;
+        }
+    }
+
+
 }
