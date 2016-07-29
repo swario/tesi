55cc38aaa2fdcc7964d372250fbe2f16111c8b5c
Crasho21 <accettacristian94@yahoo.it>
2016-07-29 13:56:07 +0200

    Aggiunto i fragment di registazione by Crasho21 13:57

--- app/src/main/java/com/example/cristian/everysale/Main2Activity.java
+++ app/src/main/java/com/example/cristian/everysale/Main2Activity.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class Main2Activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
