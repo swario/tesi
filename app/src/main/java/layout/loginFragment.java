af5d575b3e059470ce4cfb07f91b296da2439197
Giorgiboy <thegiorginator92@hotmail.it>
2016-07-29 15:29:22 +0200

    Funzione di Login client-side completa (in teoria)

--- app/src/main/java/layout/loginFragment.java
+++ app/src/main/java/layout/loginFragment.java
@@ -11,8 +11,16 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.cristian.everysale.R;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -126,6 +134,27 @@ public class loginFragment extends Fragment implements TextView.OnEditorActionLi
 
         if(name.equals("") || password.equals("")){
             return;
+        }
+        try{
+            URL url = new URL("webdev.dibris.unige.it/~S3928202/Progetto");
+            String data = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(name, "UTF-8");
+            data += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
+
+            URLConnection connection = url.openConnection();
+
+            OutputStreamWriter streamWriter = new OutputStreamWriter(connection.getOutputStream());
+            streamWriter.write(data);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String response = reader.readLine();
+
+            if(response.equals("success")){
+
+                Toast.makeText(getContext(), "Loggato con successo", Toast.LENGTH_LONG).show();
+                //codice da login (decidere come fare)
+            }
+        }
+        catch (Exception e){
         }
     }
 
