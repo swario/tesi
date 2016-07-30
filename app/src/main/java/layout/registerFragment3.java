d67a5cae3db13158076b5d1c7dac2015bcc71c70
Giorgiboy <thegiorginator92@hotmail.it>
2016-07-30 16:53:17 +0200

    Inizio (molto vago) della funzione di registrazione

--- app/src/main/java/layout/registerFragment3.java
+++ app/src/main/java/layout/registerFragment3.java
@@ -17,11 +17,8 @@ import com.example.cristian.everysale.asincRegister;
 
 public class registerFragment3 extends Fragment implements OnClickListener {
 
-    private OnFragmentInteractionListener mListener;
     private SharedPreferences savedValues;
 
-    public registerFragment3() {
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,40 +38,13 @@ public class registerFragment3 extends Fragment implements OnClickListener {
         return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
 
             case R.id.backButton:
-                getFragmentManager().beginTransaction().replace(R.id.frame_container, new registerFragment3()).addToBackStack(null).commit();
+                getFragmentManager().beginTransaction().replace(R.id.frame_container, new registerFragment2()).addToBackStack(null).commit();
                 break;
 
             case R.id.registerSubmitButton:
