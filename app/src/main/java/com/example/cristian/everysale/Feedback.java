3c0cccab7e27dd4f8d43b649ee69ccfeb35f7e2b
Giorgiboy <thegiorginator92@hotmail.it>
2016-07-31 16:56:52 +0200

    Inizio (molto vago) della funzione di registrazione

--- app/src/main/java/com/example/cristian/everysale/Feedback.java
+++ app/src/main/java/com/example/cristian/everysale/Feedback.java
@@ -5,18 +5,24 @@ package com.example.cristian.everysale;
  */
 public class Feedback {
 
-    private String description;
+    private String feedbackText;
     private int userId;
     private String userName;
 
     public Feedback(String description, int userId, String userName){
-        this.description = description;
+        this.feedbackText = description;
         this.userId = userId;
         this.userName = userName;
     }
 
-    public String getDescription(){ return description; }
+    public Feedback(){}
+
+    public String getDescription(){ return feedbackText; }
     public String getUserName(){ return userName; }
     public int getUserId(){ return  userId; }
+
+    public void setfeedbackText(String feedbackText){ this.feedbackText = feedbackText; }
+    public void setUserId(int id){ this.userId = id;}
+    public void setUserName(String name){ this.userName = name; }
 
 }
