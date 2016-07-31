7186bf532c85db43ae79fb331bebdb2dea9a3c72
Crasho21 <accettacristian94@yahoo.it>
2016-07-31 12:41:07 +0200

    register fragment bugfix by Crasho21 12:42

--- app/src/main/java/com/example/cristian/everysale/MainActivity.java
+++ app/src/main/java/com/example/cristian/everysale/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.cristian.everysale;
 
+import android.content.Intent;
+import android.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import layout.mainFragment.OnFragmentInteractionListener;
 import layout.mainFragment;
 
@@ -12,13 +16,54 @@ public class MainActivity extends AppCompatActivity implements OnFragmentInterac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setSubtitle("mytest");
+        actionBar.setTitle("vogella.com");
+        actionBar.setLogo(R.drawable.ic_menu_camera);
+        actionBar.setDisplayUseLogoEnabled(true);
 
-        getSupportFragmentManager().beginTransaction().add(R.id.frame_container, new mainFragment()).addToBackStack(null).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.frame_container, new mainFragment()).commit();
     }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
 
     }
-    
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)  {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Intent backHome = new Intent(this, MainActivity.class);
+            startActivity(backHome);
+            this.finish();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Fragment fragment1 = getFragmentManager().findFragmentById(R.layout.fragment_register_1);
+        if(fragment1!=null){
+            Intent backHome = new Intent(this, MainActivity.class);
+            startActivity(backHome);
+            this.finish();
+            super.onBackPressed();
+        }
+        Fragment fragment2 = getFragmentManager().findFragmentById(R.layout.fragment_register_2);
+        if(fragment2!=null){
+            Intent backHome = new Intent(this, MainActivity.class);
+            startActivity(backHome);
+            this.finish();
+            super.onBackPressed();
+        }
+        Fragment fragment3 = getFragmentManager().findFragmentById(R.layout.fragment_register_3);
+        if(fragment3!=null){
+            Intent backHome = new Intent(this, MainActivity.class);
+            startActivity(backHome);
+            this.finish();
+            super.onBackPressed();
+        }
+    }
 }
