97e3ae543d48a1c60d0cb7f1d62bfa976e7086f6
swario <pentolaq-o-ulix@hotmail.it>
2016-08-01 17:13:56 +0200

    Merge branch 'master' of C:\Users\ulisse\university\PAAL\tesi\EverySale-master with conflicts.

--- app/src/main/java/com/example/cristian/everysale/fragment/registerFragment3.java
+++ app/src/main/java/com/example/cristian/everysale/fragment/registerFragment3.java
@@ -1,28 +1,54 @@
 package com.example.cristian.everysale.fragment;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.cristian.everysale.R;
 import com.example.cristian.everysale.asincronousTasks.asincRegister;
 
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStreamReader;
+import java.util.Date;
+
 public class registerFragment3 extends Fragment implements OnClickListener {
 
-    private SharedPreferences savedValues;
+    /*//image picker
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    private Uri fileUri; // file url to store image/video
+    */
 
     private Button imageButton;
     private CheckBox dataAllowCheckbox;
+    private ImageView imageView;
+    private SharedPreferences savedValues;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
@@ -32,7 +58,7 @@ public class registerFragment3 extends Fragment implements OnClickListener {
         View view = inflater.inflate(R.layout.fragment_register_3, container, false);
 
         savedValues = getActivity().getSharedPreferences("SavedValues", getActivity().MODE_PRIVATE);
-
+        imageView = (ImageView) view.findViewById(R.id.imageView1);
         imageButton = (Button) view.findViewById(R.id.imageButton);
         dataAllowCheckbox = (CheckBox) view.findViewById(R.id.dataAllowCheckBox);
 
@@ -48,7 +74,8 @@ public class registerFragment3 extends Fragment implements OnClickListener {
         switch (v.getId()){
 
             case R.id.imageButton:
-
+                // capture picture
+                captureImage();
                 break;
 
             case R.id.backButton:
@@ -130,6 +157,102 @@ public class registerFragment3 extends Fragment implements OnClickListener {
         }
     }
 
+    //image picker
+    /**
+    //Checking device has camera hardware or not
+    private boolean isDeviceSupportCamera() {
+        if (getActivity().getApplicationContext().getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_CAMERA)) {
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    //Launching camera app to capture image
+    private void captureImage() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+        // start the image capture Intent
+        startActivityForResult(intent, CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
+    }
+
+    //Receiving activity result method will be called after closing the camera
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // if the result is capturing Image
+        if (requestCode == CAMERA_CAPTURE_IMAGE_REQUEST_CODE) {
+            if (resultCode == getActivity().RESULT_OK) {
+                // successfully captured the image
+                // launching upload activity
+                //launchUploadActivity(true);
+            } else if (resultCode == getActivity().RESULT_CANCELED) {
+                // user cancelled Image capture
+                Toast.makeText(getActivity().getApplicationContext(),
+                        "User cancelled image capture", Toast.LENGTH_SHORT)
+                        .show();
+            } else {
+                // failed to capture image
+                Toast.makeText(getActivity().getApplicationContext(),
+                        "Sorry! Failed to capture image", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+    // Create a file Uri for saving an image or video
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    // Create a File for saving an image or video
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new   File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        }else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+    */
+
+    //end image picker
+
+    //image picker2
+
+
+
+
+    //end image picke2
+
     private int getCitySpinner(int region){
 
         switch(region){
@@ -177,4 +300,6 @@ public class registerFragment3 extends Fragment implements OnClickListener {
                 return R.array.fregister2_region0_spinner;
         }
     }
+
+
 }
