01874d50fb4d20b79c8bdc7bf169cbbe6f0cbe6c
Giorgiboy <thegiorginator92@hotmail.it>
2016-08-01 09:59:40 +0200

    Piccoli cambiamenti nelle cartelle delle risorse

--- app/src/main/java/com/example/cristian/everysale/BaseClasses/SearchResponse.java
+++ app/src/main/java/com/example/cristian/everysale/BaseClasses/SearchResponse.java
@@ -15,8 +15,21 @@ public class SearchResponse {
     public void setSearchKey(String sk) { this.searchKey = sk; }
     public void setInsertionCount(int count) { this.insertionCount = count; }
 
+    public int getInsertionCount(){ return insertionCount; }
+    public Insertion getInsertion( int pos){ return insertions.get(pos); }
+
     public void addInsertion(Insertion insertion){
 
         insertions.add(insertion);
+    }
+
+    public void merge(SearchResponse toBeMerged){
+
+        int number = toBeMerged.getInsertionCount();
+        for(int i = 0; i < number; i++){
+
+            insertions.add(toBeMerged.getInsertion(i));
+        }
+        this.insertionCount += number;
     }
 }
