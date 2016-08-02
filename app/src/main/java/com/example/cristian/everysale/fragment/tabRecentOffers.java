ec542e50fa72739857a3157fe4a363228e6c5ac1
Giorgiboy <thegiorginator92@hotmail.it>
2016-08-02 14:42:46 +0200

    Download di una ricerca completato, come refresh per la list view

--- app/src/main/java/com/example/cristian/everysale/fragment/tabRecentOffers.java
+++ app/src/main/java/com/example/cristian/everysale/fragment/tabRecentOffers.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,15 +12,17 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import com.example.cristian.everysale.BaseClasses.InsertionPreview;
 import com.example.cristian.everysale.BaseClasses.SearchResponse;
 import com.example.cristian.everysale.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class tabRecentOffers extends ListFragment {
+public class tabRecentOffers extends ListFragment implements SwipeRefreshLayout.OnRefreshListener{
 
     private SearchResponse searchResponse;
+    private SwipeRefreshLayout refreshLayout;
 
     private ListView itemsListView;
     private View view;
@@ -27,6 +30,10 @@ public class tabRecentOffers extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.recent_listview,container,false);
+
+        refreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.recent_refresh_layout);
+        refreshLayout.setOnRefreshListener(this);
+
         searchResponse = null;
         return view;
     }
@@ -34,12 +41,13 @@ public class tabRecentOffers extends ListFragment {
     @Override
     public void onResume(){
         super.onResume();
-        setListView();
     }
 
     private void setListView(){
         ArrayList<HashMap<String, String>> data = new ArrayList<>();
-        for(int i=0; i<10; i++){
+        for(int i=0; i < searchResponse.getInsertionCount(); i++){
+
+            InsertionPreview preview = searchResponse.getInsertion(i);
             HashMap<String, String> map = new HashMap<>();
             map.put("icon", "http://webdev.dibris.unige.it/~S3928202/Progetto/img/favicon.jpg");
             map.put("title", "Titolo1");
@@ -63,6 +71,13 @@ public class tabRecentOffers extends ListFragment {
         else{
             this.searchResponse.merge(searchResponse);
         }
+        setListView();
     }
 
+    @Override
+    public void onRefresh() {
+
+        searchResponse = null;
+
+    }
 }
