36e00973f1dc0efb0af32690e5d1b4eeacca2fdf
swario <pentolaq-o-ulix@hotmail.it>
2016-08-05 17:09:46 +0200

    s 12.31 aggiustamenti layout di visualizzazione e modifica profilo
    creato layout nuova inserzione

--- app/src/main/java/com/example/cristian/everysale/Main2Activity.java
+++ app/src/main/java/com/example/cristian/everysale/Main2Activity.java
@@ -2,19 +2,18 @@ package com.example.cristian.everysale;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.NavigationView.OnNavigationItemSelectedListener;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v4.widget.DrawerLayout.DrawerListener;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.cristian.everysale.Listeners.MenuListener;
 import com.example.cristian.everysale.fragment.viewPagerAdapter;
@@ -33,6 +32,11 @@ public class Main2Activity extends AppCompatActivity {
     SlidingTabLayout tabs;
     CharSequence Titles[]={"In zona","Recenti","preferiti"};
     int Numboftabs =3;
+
+    //icona toolbar
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,6 +86,13 @@ public class Main2Activity extends AppCompatActivity {
         MenuListener menuListener = new MenuListener(this, navigationView, drawerLayout);
         drawerLayout.addDrawerListener(menuListener);
         navigationView.setNavigationItemSelectedListener(menuListener);
+
+        //icona toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+        setupDrawer();
     }
 
 
@@ -92,6 +103,7 @@ public class Main2Activity extends AppCompatActivity {
         return true;
     }
 
+    //funzionamento menu laterale
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -99,18 +111,51 @@ public class Main2Activity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.nav_home) {
-            Intent intent = new Intent(this, Main2Activity.class);
-            startActivity(intent);
-            this.finish();
-        }else if (id == R.id.nav_disconnect) {
-            getSharedPreferences("SavedValues", MODE_PRIVATE).edit().clear().commit();
-            Intent intent = new Intent(this, StartActivity.class);
-            startActivity(intent);
-            this.finish();
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
+        // Handle your other action bar items...
 
         return super.onOptionsItemSelected(item);
+
     }
+
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            /**
+             * Called when a drawer has settled in a completely open state.
+             */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /**
+             * Called when a drawer has settled in a completely closed state.
+             */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+
 }
