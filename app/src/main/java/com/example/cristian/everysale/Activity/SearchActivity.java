e8ece730124526bbe80279937b1a6ab283bff083
swario <pentolaq-o-ulix@hotmail.it>
2016-08-07 18:18:47 +0200

    searchbar

--- app/src/main/java/com/example/cristian/everysale/Activity/SearchActivity.java
+++ app/src/main/java/com/example/cristian/everysale/Activity/SearchActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.SearchView;
 
 import com.example.cristian.everysale.R;
 
@@ -16,6 +18,12 @@ public class SearchActivity extends navigationDrawerActivity {
         View contentView = inflater.inflate(R.layout.activity_search, null, false);
         drawerLayout.addView(contentView,0);
 
+        SearchView searchView = (SearchView) findViewById(R.id.searchView);
 
+        //setto focus e apro tastiera
+        searchView.requestFocus();
+        InputMethodManager inputMethodManager =
+                (InputMethodManager) this.getSystemService(this.INPUT_METHOD_SERVICE);
+        inputMethodManager.showSoftInput(searchView, InputMethodManager.SHOW_IMPLICIT);
     }
 }
