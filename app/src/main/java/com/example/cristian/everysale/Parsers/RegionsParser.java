e899152b3955f1fadae7fe8abf7a5dadcf0c658c
Crasho21 <accettacristian94@yahoo.it>
2016-08-07 11:34:59 +0200

    Modifiche a region Parser by Crasho21 11:30

--- app/src/main/java/com/example/cristian/everysale/Parsers/RegionsParser.java
+++ app/src/main/java/com/example/cristian/everysale/Parsers/RegionsParser.java
@@ -1,14 +1,27 @@
 package com.example.cristian.everysale.Parsers;
 
+import com.example.cristian.everysale.BaseClasses.Region;
+
 import org.xml.sax.helpers.DefaultHandler;
+
+import java.util.ArrayList;
 
 /**
  * Created by Cristian on 07/08/2016.
  */
 public class RegionsParser extends DefaultHandler {
 
-    private boolean isRegion;
-    private boolean isRegionCode;
-    private boolean isRegionName;
+    private boolean isRegion = false;
+    private boolean isRegionCode = false;
+    private boolean isRegionName = false;
+
+    private Region region;
+    private ArrayList<Region> regions;
+
+    public RegionsParser(){
+        region = new Region();
+        regions = new ArrayList<>();
+    }
+
 
 }
