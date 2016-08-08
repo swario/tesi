9ac19b9fd44e4051301d5ef17344c77fba404685
swario <pentolaq-o-ulix@hotmail.it>
2016-08-08 13:18:49 +0200

    creata activity show profile

---
+++ app/src/main/java/com/example/cristian/everysale/Activity/ShowProfileActivity.java
@@ -0,0 +1,20 @@
+package com.example.cristian.everysale.Activity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.example.cristian.everysale.R;
+
+public class ShowProfileActivity extends navigationDrawerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View contentView = inflater.inflate(R.layout.activity_show_profile , null, false);
+        drawerLayout.addView(contentView, 0);
+
+    }
+}
