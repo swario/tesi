f68122b1651c9807a0f41cd57979de783a982231
Giorgiboy <thegiorginator92@hotmail.it>
2016-08-09 12:15:23 +0200

    Cambiamenti nelle cartelle

--- app/src/main/java/com/example/cristian/everysale/Interfaces/ListTab.java
+++ app/src/main/java/com/example/cristian/everysale/Interfaces/ListTab.java
@@ -1,6 +1,8 @@
 package com.example.cristian.everysale.Interfaces;
 
 
+import com.example.cristian.everysale.BaseClasses.SearchResponse;
+
 public interface ListTab {
 
     public void goToInsertion(long pos);
